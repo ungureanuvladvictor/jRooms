317c5e00faebf69cbb079c6b19dcaeb1e5308326
Dmitrii Cucleschin <dmitrii.cucleschin@gmail.com>
2015-01-25 23:05:17 +0100

    a bit more examples; db gitignored

--- app.js
+++ app.js
@@ -11,6 +11,10 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, '(!!!) MongoDB connection error:'));
 db.once('open', function() { console.log("Connected to MongoDB database at mongodb://" + config.dbhost + '/' + config.dbname + "..."); });
 
+// app.get('/user', function(req, res) {
+// 	res.send('GET request to user');
+// })
+
 var server = app.listen(3000, function() {
   	console.log("Listening at port " + server.address().port + "...");
 });

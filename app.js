e784a2a5bfacc3e7fc9987506454ffc106ae10b7
filipbitola <filipstankovski@yahoo.com>
2015-01-26 00:33:41 +0100

    stuff

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var config = require('./config.json');
+var login = require('./routes/login');
 
 var express = require('express');
 var app = express();
@@ -11,9 +12,18 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, '(!!!) MongoDB connection error:'));
 db.once('open', function() { console.log("Connected to MongoDB database at mongodb://" + config.dbhost + '/' + config.dbname + "..."); });
 
-// app.get('/user', function(req, res) {
-// 	res.send('GET request to user');
-// })
+app.get('/login/:username/:password', function(user, pass) {
+
+	// Get actual values
+	var user = "fstankovsk";
+	var pass = "aaa";
+	return login.login(user, pass);
+});
+app.get('/logout/:token', login.logout);
+
+app.get('/user', function(req, res) {
+	res.send('GET request to user');
+});
 
 var server = app.listen(3000, function() {
   	console.log("Listening at port " + server.address().port + "...");
---
+++ routes/login.js
@@ -0,0 +1,10 @@
+
+
+exports.login = function(username, password) {
+	return "sometoken";
+}
+
+exports.logout = function(token_string) {
+	// delete token_string from database
+	return "";
+}

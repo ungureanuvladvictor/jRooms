7a3a178c2e2b27a50d278b217c1ddf73c05c8200
Dmitrii Cucleschin <dmitrii.cucleschin@gmail.com>
2015-01-25 22:51:11 +0100

    connecting to mongodb

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+var config = require('./config.json');
+
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/static'));
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://' + config.dbhost + '/' + config.dbname);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, '(!!!) MongoDB connection error:'));
+db.once('open', function() { console.log("Connected to MongoDB database at mongodb://" + config.dbhost + '/' + config.dbname + "..."); });
 
 var server = app.listen(3000, function() {
   	console.log("Listening at port " + server.address().port + "...");
